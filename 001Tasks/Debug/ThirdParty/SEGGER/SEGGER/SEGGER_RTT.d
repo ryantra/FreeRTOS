c5b4f25c8f6151be64160c8a241b2772d235f487
Rupesh Majhi <zoone.rupert@gmail.com>
2026-07-20 17:59:52 +0300

    Untrack nested build artifacts so language stats reflect C source

--- 001Tasks/Debug/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.d
+++
@@ -1,8 +0,0 @@
-ThirdParty/SEGGER/SEGGER/SEGGER_RTT.o: \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.c \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- ../ThirdParty/SEGGER/SEGGER/../Config/SEGGER_RTT_Conf.h
-
-../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-
-../ThirdParty/SEGGER/SEGGER/../Config/SEGGER_RTT_Conf.h:
--- 001Tasks/Debug/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d
+++
@@ -1,8 +0,0 @@
-ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
- ../ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
- /home/rupesh/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- /home/rupesh/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/../Config/SEGGER_RTT_Conf.h
-
-/home/rupesh/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-
-/home/rupesh/Workspace/RTOS_workspace/001Tasks/ThirdParty/SEGGER/SEGGER/../Config/SEGGER_RTT_Conf.h:
